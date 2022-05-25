365b60dea8ded823118737d16ce2a28edba48997
Lady Liliana Vasquez Saiz <lady.vasquez@globant.com>
2022-05-25 09:51:37 -0500

    Feat: Create feedback form and button components

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { useState } from 'react';
 import Header from './components/Header';
 import FeedbackList from './components/FeedbackList';
 import FeedbackStats from './components/FeedbackStats';
+import FeedbackForm from './components/FeedbackForm';
 import FeedbackData from './data/FeedbackData';
 
 function App() {
@@ -19,8 +20,9 @@ function App() {
     <>
       <Header />
       <div className="container">
-        <FeedbackStats feedback={feedback}/>
-        <FeedbackList feedback={feedback} handleDelete={deleteFeedback}/>
+        <FeedbackForm />
+        <FeedbackStats feedback={feedback} />
+        <FeedbackList feedback={feedback} handleDelete={deleteFeedback} />
       </div>
     </>
   )
