f5b1935aa614aa0acc06524642ced5a5ddcfa7bb
Lady Liliana Vasquez Saiz <lady.vasquez@globant.com>
2022-06-01 08:35:37 -0500

    Feat: create edit feedback event, useEffect, and update feedback item

--- src/context/FeedbackContext.js
+++ src/context/FeedbackContext.js
@@ -27,23 +27,46 @@ export const FeedbackProvider = ({
     },
   ]);
 
+  const [feedbackEdit, setFeedbackEdit] = useState({
+    item: {},
+    edit: false
+  });
+
+  //Add feedback
   const addFeedback = (newFeedback) => {
     //Generate an unique ID with UUID - It throws a string, not a number, that's why we need the +
     newFeedback.id = uuidv4(); 
     setFeedback([newFeedback, ...feedback])
   }
 
+  //Delete feedback
   const deleteFeedback = (id) => {
     if(window.confirm('Are you sure you want to delete?')) {
       setFeedback(feedback.filter((item) => item.id !== id))
     }
   }
 
+  //Update feedback item
+  const updateFeedback = (id, updItem) => {
+    setFeedback(feedback.map((item) => item.id === id ? { ...item, ...updItem } : item))
+  }
+
+  //Set item to be updated
+  const editFeedback = (item) => {
+    setFeedbackEdit({
+      item,
+      edit: true
+    })
+  }
+
   return <FeedbackContext.Provider 
       value={{
         feedback,
+        feedbackEdit, /* state */
         deleteFeedback,
-        addFeedback
+        addFeedback,
+        editFeedback, /* function onClick */
+        updateFeedback
       }}
     >
       {children}
